c480f6ba722371c5f30887cd2427e2cc3e200c06
yuqian <584882416@qq.com>
2022-06-29 12:35:26 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue</div>
     <Nav/>
   </div>
 </template>
@@ -10,6 +12,15 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
