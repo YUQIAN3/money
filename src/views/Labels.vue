2980453736e4c73adf20ac7ebc3fe0d91f2ff884
yuqian <584882416@qq.com>
2022-07-11 16:22:45 +0800

    展示和创建标签

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,17 +1,70 @@
 <template>
-  <div>
     <layout>
-      Labels.vue
-    </layout>
-  </div>
+      <ol class="tags">
+        <li v-for="tag in tags" :key="tag">
+          <span>{{tag}}</span>
+          <Icon name="right"/>
+        </li>
+      </ol>
+      <div class="createTag-wrapper">
+        <button class="createTag" @click="createTag">新增标签</button>
+      </div>
+      </layout>
 </template>
 
-<script>
-export default {
-  name: "Labels",
+<script lang="ts">
+
+import tagModel from '@/models/tagModel';
+import tagListModel from '@/models/tagModel';
+import Vue from 'vue';
+import Component from 'vue-class-component';
+tagModel.fetch();
+@Component
+export default class extends Vue{
+  tags=tagModel.data;
+  createTag() {
+    const name = window.prompt('请输入标签名：')
+    if (name) {
+    const message=tagListModel.create(name)
+
+        if (message === 'duplicated') {
+          window.alert('标签名重复')
+        }
+      }
+    }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.tags{
+  background: white;
+  >li{
+  min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+  }
+  .svg{
+    width: 16px;
+    height: 16px;
+    color:#666;
+    margin-right: 16px;
+
+  }
+}
+.createTag{
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper{
+  text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
 
 </style>
