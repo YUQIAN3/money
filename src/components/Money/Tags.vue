cb2b3a7406c27f5c6ebf5e88a9250e99fcd62df9
yuqian <584882416@qq.com>
2022-07-12 17:37:14 +0800

    消除tags.vue的警告

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,9 +4,9 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag"
+      <li v-for="tag in dataSource" :key="tag.id"
           :class="{selected:selectedTags.indexOf(tag)>=0}"
-     @click="toggle(tag)" >{{tag}}</li>
+     @click="toggle(tag)" >{{tag.name}}</li>
     </ul>
   </div>
 </template>
@@ -42,6 +42,7 @@ export default class Tags extends Vue{
 
 <style lang="scss" scoped>
 .tags{
+  background: white;
   display: flex;
   flex-direction: column-reverse;
   flex-grow: 1;
