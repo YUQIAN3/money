9aba54691eeb09ef51a978a5388d4a5511944d83
yuqian <584882416@qq.com>
2022-07-13 11:00:39 +0800

    展示tag的name

--- src/views/EditLable.vue
+++ src/views/EditLable.vue
@@ -1,10 +1,16 @@
 <template>
 <layout>
-  <div>
-    <Icon name="left"/>
-    <span>编辑标签</span>
+  <div class="navBar">
+    <Icon class="leftIcon"   name="left"/>
+    <span class="title">编辑标签</span>
+    <span class="rightIcon"></span>
   </div>
-<Notes field-name="标签名" placeholder="请输入标签名"/>
+  <div class="form-wrapper">
+<FormItem :value="tag.name"  field-name="标签名" placeholder="请输入标签名"/>
+  </div>
+  <div class="button-wrapper">
+  <Button>删除标签</Button>
+  </div>
 </layout>
 </template>
 
@@ -12,16 +18,19 @@
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator';
 import tagModel from '@/models/tagModel';
-import Notes from '@/components/Money/Notes.vue';
+import formItem from '@/components/Money/FormItem.vue';
+import Button from '@/components/Money/Button.vue';
 @Component({
-  components: {Notes}
+  components: {Button, formItem}
 })
 export default class EditLabel extends Vue {
+  tag?:{id:string,name:string}=undefined;
   created(){
 const id=this.$route.params.id;
 tagModel.fetch();
 const tags=tagModel.data;
 const tag=tags.filter(t=>t.id===id)[0];
+this.tag=tag;
 if(tag){
   console.log(tag);
 }else{
@@ -31,6 +40,34 @@ if(tag){
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.navBar{
+  background: white;
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  >.title{
 
+  }
+  >.leftIcon{
+   height: 24px;
+    width: 24px;
+  }
+  >.rightIcon{
+    height: 24px;
+    width: 24px;
+  }
+}
+.form-wrapper{
+  background: white;
+  margin-top: 8px;
+}
+.button-wrapper{
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16 px;
+}
 </style>
