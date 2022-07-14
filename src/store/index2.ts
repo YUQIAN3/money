ee8d623ecb063955b29c6edd9ea6591aad0ad1a9
yuqian <584882416@qq.com>
2022-07-14 17:04:49 +0800

    将所有的数据操作封装在store里

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,28 +1,11 @@
-import recordListModel from '@/models/recordListModel';
-import tagModel from '@/models/tagModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
+
 
 const store={
     //record store
-    recordList:recordListModel.fetch(),
-    createRecord:(record:RecordItem)=> recordListModel.create(record),
+...recordStore,
 //tag store
-    tagList:tagModel.fetch(),
-    findTag(id:string){
-        return this.tagList.filter(t=>t.id===id)[0];
-    },
-    createTag:(name:string)=>{
-        const message=tagModel.create(name)
-        if (message === 'duplicated') {
-            window.alert('标签名重复')
-        }else if(message==='success'){
-            window.alert('添加成功')
-        }
-    },
-    removeTag:(id:string)=>{
-        return tagModel.remove(id);
-    },
-    updateTag:(id:string,name:string)=>{
-        return tagModel.update(id,name)
-    }
+    ...tagStore
 }
 export default store;
