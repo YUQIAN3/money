5441ba6846a03583fc674def0fc7b2c6d59e45e0
yuqian <584882416@qq.com>
2022-07-20 17:03:18 +0800

    修复了刷新变成404的bug

--- src/views/EditLable.vue
+++ src/views/EditLable.vue
@@ -22,30 +22,34 @@ import Vue from 'vue'
 import {Component} from 'vue-property-decorator';
 import formItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Money/Button.vue';
-import store from '@/store/index2';
 @Component({
   components: {Button, formItem}
 })
 export default class EditLabel extends Vue {
-    tag?:Tag=undefined
-  created(){
-    this.tag=store.findTag(this.$route.params.id);
-  if(!this.tag){
-
-}
+    get tag(){
+      return this.$store.state.currentTag;
+    }
+  created() {
+    const id = this.$route.params.id;
+    console.log(id);
+    this.$store.commit('fetchTags');
+    this.$store.commit('setCurrentTag', id);
+    if (!this.tag) {
+      console.log('no tag');
+      this.$router.replace('/404');
+    }else{
+      console.log('has tag');
+    }
   }
   update(name:string){
     if(this.tag){
-      store.updateTag(this.tag.id,name)
+      this.$store.commit('updateTag',
+         {id:this.tag.id,name:name})
     }
   }
-  remove(){
-    if(this.tag){
-     if(store.removeTag(this.tag.id)){
-       this.$router.back();
-     }else{
-       window.alert('删除失败')
-     }
+  remove() {
+    if (this.tag) {
+  this.$store.commit('removeTag',this.tag.id)
     }
   }
   goBack(){
