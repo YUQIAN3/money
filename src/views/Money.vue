036ea4f9a139ff947a95043fb0c0f54dac12277c
yuqian <584882416@qq.com>
2022-07-26 10:41:10 +0800

    在money.vue里面使用Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,31 +2,35 @@
       <layout class-prefix="layout">
         {{record}}
         <NumberPad @update:value="onUpDateAmount" @submit="saveRecord"/>
-        <Type :value.sync="record.type" />
+<!--        <Types :value.sync="record.type"/>-->
+        <Tabs :data-source=" recordTypeList"
+              :value.sync=" record.type"/>
         <div class="notes">
           <formItem field-name="标签名"
                     placeholder="请输入标签名"
                @update:value="onUpDateTagsNotes"/>
         </div>
-
         <Tags/>
       </layout>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Type from '@/components/Money/Types.vue';
+import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import formItem from '@/components/Money/FormItem.vue';
 import {Component} from 'vue-property-decorator';
+import recordTypeList from '@/constants/recordTypeList';
+import Tabs from '@/components/Tabs.vue';
 @Component({
-  components:{Tags,formItem,Type,NumberPad},
+  components:{Types, Tabs, Tags,formItem,Type: Types,NumberPad},
 })
 
 export default class Money extends Vue{
   get recordList(){
     return this.$store.state.recordlist;
   }
+  recordTypeList=recordTypeList;
   record:RecordItem={
     tags:[],notes:'',type:'-',amount:0
   }
