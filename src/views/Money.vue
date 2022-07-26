1152d2354e2e7cc0e903f717b422e64885a0a41c
yuqian <584882416@qq.com>
2022-07-26 10:57:39 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,14 +16,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import formItem from '@/components/Money/FormItem.vue';
 import {Component} from 'vue-property-decorator';
 import recordTypeList from '@/constants/recordTypeList';
 import Tabs from '@/components/Tabs.vue';
 @Component({
-  components:{Types, Tabs, Tags,formItem,Type: Types,NumberPad},
+  components:{ Tabs, Tags,formItem,NumberPad},
 })
 
 export default class Money extends Vue{
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,18 +1,37 @@
 <template>
-  <div>
     <layout>
-      Statistics.vue
+      <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+      <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     </layout>
-  </div>
 </template>
 
-<script>
-export default {
-  name: "Statistics",
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constants/intervalList';
+import recordTypeList from '@/constants/recordTypeList';
+@Component({
+  components: {Tabs}
+})
+export default class Statistics extends Vue {
+type='-';
+interval='day'
+intervalList=intervalList;
+  recordTypeList=recordTypeList;
 
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+::v-deep .type-tabs-item{
+  background: white;
+  &.selected{
+    background: #c4c4c4;
+  }
+  &::after{
+    display: none;
+  }
+}
 
 </style>
